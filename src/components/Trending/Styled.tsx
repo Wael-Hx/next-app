17ca9d5a787ca2ce85ee7e722ad3d2e53aead939
Wael-Hx <wael.lhb@gmail.com>
2021-04-30 22:22:03 +0100

    set cursor style while grabbing

--- src/components/Trending/Styled.tsx
+++ src/components/Trending/Styled.tsx
@@ -5,7 +5,7 @@ export const Section = styled.section`
   position: relative;
   box-shadow: 0 0 80px rgba(0, 0, 0, 0.9) inset;
   width: 93vw;
-  height: 80vh;
+  height: 40vw;
   margin: auto;
   display: flex;
   overflow-x: scroll;
@@ -19,11 +19,12 @@ export const Section = styled.section`
 
 export const MovieCover = styled(Image)`
   scroll-snap-align: start;
+  user-select: none;
 `;
 
 export const Controls = styled.ul`
   position: fixed;
-  bottom: calc(100vh - 88vh);
+  bottom: calc(100vh - 41vw);
   right: 50%;
   transform: translateX(50%);
   font-size: 2rem;
@@ -37,11 +38,13 @@ export const Controls = styled.ul`
   }
 `;
 
-export const Shadow = styled.div`
+export const Shadow = styled.div<{ grabState: "grab" | "grabbing" }>`
   position: absolute;
   top: 0;
   bottom: 0;
   width: 100%;
   height: 100%;
   box-shadow: 0 0 80px 10px rgba(0, 0, 0, 0.9) inset;
+  user-select: none;
+  cursor: ${(props) => props.grabState};
 `;
