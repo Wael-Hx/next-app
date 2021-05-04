ba7b1985e72448bc8854113175e256c2d90c7a84
Wael-Hx <wael.lhb@gmail.com>
2021-05-04 21:25:59 +0100

    dispaly studios

---
+++ src/components/studios/Studios.tsx
@@ -0,0 +1,39 @@
+import { useColorModeValue } from "@chakra-ui/color-mode";
+import { Grid } from "@chakra-ui/layout";
+import { DC, Disney, Marvel, Pixar } from "../icons/Icons";
+import NewSection from "../NewSection";
+import StudioLogo from "./StudioLogo";
+
+const Studios = () => {
+  const color = useColorModeValue("black", "white");
+  return (
+    <NewSection title="Studios">
+      <Grid
+        mt="5"
+        alignItems="center"
+        gap="3"
+        justifyItems="center"
+        gridTemplateColumns="repeat(auto-fit, minmax(240px, 1fr))"
+      >
+        <StudioLogo
+          backgroundAnimation="/media/videos/DC.mp4"
+          logo={<DC boxSize="20" zIndex={2} />}
+        />
+        <StudioLogo
+          backgroundAnimation="/media/videos/Marvel.mp4"
+          logo={<Marvel boxSize="28" zIndex={2} />}
+        />
+        <StudioLogo
+          backgroundAnimation="/media/videos/Pixar.mp4"
+          logo={<Pixar fill={color} boxSize="28" zIndex={2} />}
+        />
+        <StudioLogo
+          backgroundAnimation="/media/videos/Disney.mp4"
+          logo={<Disney fill={color} boxSize="28" zIndex={2} />}
+        />
+      </Grid>
+    </NewSection>
+  );
+};
+
+export default Studios;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
+import MoviesList from "../components/movies/MoviesList";
 import Navbar from "../components/nav/Navbar";
+import Studios from "../components/studios/Studios";
 import Trending from "../components/Trending/Trending";
 
-const Index = () => (
-  <>
-    <Navbar />
-    <Trending />
-  </>
-);
+const Index = () => {
+  const limit = 5;
+
+  return (
+    <>
+      <Navbar />
+      <Trending limit={limit} />
+      <Studios />
+      <MoviesList />
+    </>
+  );
+};
 
 export default Index;
