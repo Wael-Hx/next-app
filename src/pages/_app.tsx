b6f36f568ab33803d4434ac2872c760f12c48bf0
Wael-Hx <wael.lhb@gmail.com>
2021-05-27 15:26:05 +0100

    apollo client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,17 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import theme from "../theme";
 import "../styles/globals.css";
+import { useApollo } from "../graphql/client";
+import { ApolloProvider } from "@apollo/client";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={apolloClient}>
+      <ChakraProvider resetCSS theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
