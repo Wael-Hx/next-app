08eb5c7b2538da305a3cca6f47cdffdc194a5fbd
Wael-Hx <wael.lhb@gmail.com>
2021-05-29 22:51:43 +0100

    disale body parsing

--- src/pages/api/graphql.ts
+++ src/pages/api/graphql.ts
@@ -3,4 +3,10 @@ import { schema } from "../../graphql/schema";
 
 const apolloServer = new ApolloServer({ schema });
 
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
 export default apolloServer.createHandler({ path: "/api/graphql" });
