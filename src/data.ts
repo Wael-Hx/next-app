79e24771f6f68ee2c1660f6fddfc2dd6dadb7a21
Wael-Hx <wael.lhb@gmail.com>
2021-06-08 08:52:13 +0100

    data

---
+++ src/data.ts
@@ -0,0 +1,223 @@
+import { Movie } from "./types";
+
+export const data: Movie[] = [
+  {
+    name: "Mortal Kombat",
+    cover:
+      "https://www.themoviedb.org/t/p/original/9yBVqNruk6Ykrwc32qrK2TIE5xw.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/xGuOF1T3WmPsAcQEQJfnG7Ud9f8.jpg",
+    description:
+      "Washed-up MMA fighter Cole Young, unaware of his heritage, and hunted by Emperor Shang Tsung's best warrior, Sub-Zero, seeks out and trains with Earth's greatest champions as he prepares to stand against the enemies of Outworld in a high stakes battle for the universe.",
+    genres: ["Fantasy", "Action", "Adventure", "Fiction", "Thriller"],
+    released: "04/23/2021 (US)",
+    rating: "R",
+    score: {
+      imdb: 6.3,
+      mc: 44,
+      rt: 54,
+    },
+    trailer: "NYH2sLid0Zc",
+    runtime: "1h 50m",
+  },
+  {
+    name: "Zack Snyder's Justice League",
+    cover:
+      "https://www.themoviedb.org/t/p/original/9WoLTsRgrupkvCgs3bhsl2EjLhs.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/wFl2rumfzQJzTf50WqmK7zDg6Aa.jpg",
+    description:
+      "Determined to ensure Superman's ultimate sacrifice was not in vain, Bruce Wayne aligns forces with Diana Prince with plans to recruit a team of metahumans to protect the world from an approaching threat of catastrophic proportions.",
+    genres: ["Action", "Adventure", "Fantasy", "Science Fiction"],
+    released: "03/18/2021 (US)",
+    rating: "R",
+    score: {
+      imdb: 8.1,
+      mc: 54,
+      rt: 71,
+    },
+    trailer: "4ZceaOc_V9M",
+    runtime: "4h 2m",
+  },
+  {
+    name: "Raya and the Last Dragon",
+    cover:
+      "https://www.themoviedb.org/t/p/original/zmX7ObPaRfiCmY6LMalUb1bJDKj.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/lPsD10PP4rgUGiGR4CCXA6iY0QQ.jpg",
+    description:
+      "Long ago, in the fantasy world of Kumandra, humans and dragons lived together in harmony. But when an evil force threatened the land, the dragons sacrificed themselves to save humanity. Now, 500 years later, that same evil has returned and it’s up to a lone warrior, Raya, to track down the legendary last dragon to restore the fractured land and its divided people.",
+    genres: ["Animation", "Adventure", "Fantasy", "Family", "Action"],
+    released: "03/05/2021 (US)",
+    rating: "PG",
+    score: {
+      imdb: 7.4,
+      mc: 75,
+      rt: 0,
+    },
+    trailer: "3UFWsEY8Hdc",
+    runtime: "1h 47m",
+  },
+  {
+    name: "Nobody",
+    cover:
+      "https://www.themoviedb.org/t/p/original/6zbKgwgaaCyyBXE4Sun4oWQfQmi.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/oBgWY00bEFeZ9N25wWVyuQddbAo.jpg",
+    description: `Hutch Mansell, a suburban dad, overlooked husband, nothing neighbor — a "nobody." When two thieves break into his home one night, Hutch's unknown long-simmering rage is ignited and propels him on a brutal path that will uncover dark secrets he fought to leave behind.`,
+    genres: ["Action", "Thriller", "Crime"],
+    released: "03/26/2021 (US)",
+    rating: "R",
+    score: {
+      imdb: 7.4,
+      mc: 63,
+      rt: 83,
+    },
+    trailer: "wZti8QKBWPo",
+    runtime: "1h 32m",
+  },
+  {
+    name: "Godzilla vs. Kong",
+    rating: "PG-13",
+    released: "03/31/2021 (US)",
+    genres: ["Science Fiction", "Action", "Drama"],
+    runtime: "1h 53m",
+    description: `In a time when monsters walk the Earth, humanity’s fight for its future sets Godzilla and Kong on a collision course that will see the two most powerful forces of nature on the planet collide in a spectacular battle for the ages.`,
+    cover:
+      "https://www.themoviedb.org/t/p/original/frLQAZmciPWYqIIlIacwMXPMgiu.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/pgqgaUx1cJb5oZQQ5v0tNARCeBp.jpg",
+    score: {
+      imdb: 6.5,
+      mc: 0,
+      rt: 75,
+    },
+    trailer: "odM92ap8_c0",
+  },
+  {
+    name: "Riders of Justice",
+    cover:
+      "https://www.themoviedb.org/t/p/original/7XZsVVnqTsMJXg3J4CHzeV2Lro9.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/yBi0eNLKqqB6bOK9dKBb56Q69i1.jpg",
+    description: `Markus, a deployed military man, has to go home to his teenage daughter, Mathilde, when his wife dies in a tragic train accident. It seems to be plain bad luck - but it turns out that it might have been a carefully orchestrated assassination, which his wife ended up being a random casualty of.`,
+    genres: ["Comedy", "Action", "Drama"],
+    released: "05/21/2021 (US)",
+    runtime: "1h 56m",
+    rating: "NR",
+    score: {
+      imdb: 7.7,
+      rt: 88,
+      mc: 0,
+    },
+    trailer: "ZQSQ-D5Q4gM",
+  },
+  {
+    name: "Tenet",
+    cover:
+      "https://www.themoviedb.org/t/p/original/yY76zq9XSuJ4nWyPDuwkdV7Wt0c.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/k68nPLbIST6NP96JmTxmZijEvCA.jpg",
+    description: `Armed with only one word - Tenet - and fighting for the survival of the entire world, the Protagonist journeys through a twilight world of international espionage on a mission that will unfold in something beyond real time.`,
+    genres: ["Action", "Thriller", "Science Fiction"],
+    released: "09/03/2020 (US)",
+    rating: "PG-13",
+    runtime: "2h 30m",
+    score: {
+      imdb: 7.4,
+      mc: 69,
+      rt: 70,
+    },
+    trailer: "L3pk_TBkihU",
+  },
+  {
+    name: "A Quiet Place Part II",
+    rating: "PG-13",
+    released: "05/28/2021 (US)",
+    genres: ["Drama", "Science Fiction", "Thriller"],
+    runtime: "1h 58m",
+    description: `Following the events at home, the Abbott family now face the terrors of the outside world. Forced to venture into the unknown, they realize that the creatures that hunt by sound are not the only threats that lurk beyond the sand path.`,
+    trailer: "XEMwSdne6UE",
+    cover:
+      "https://www.themoviedb.org/t/p/original/rd2lIZcsE3Fdhh9xmmkccByBjFF.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/4q2hz2m8hubgvijz8Ez0T2Os2Yv.jpg",
+    score: {
+      imdb: 7.9,
+      mc: 71,
+      rt: 91,
+    },
+  },
+  {
+    name: "Tom Clancy's Without Remorse",
+    cover:
+      "https://www.themoviedb.org/t/p/original/wKzBu4FlFE3YJiHs2MbugGijli2.jpg",
+    description: `An elite Navy SEAL uncovers an international conspiracy while seeking justice for the murder of his pregnant wife.`,
+    genres: ["Action", "Adventure", "Thriller", "War"],
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/rEm96ib0sPiZBADNKBHKBv5bve9.jpg",
+    rating: "R",
+    released: "04/29/2021 (US)",
+    runtime: "1h 49m",
+    score: {
+      imdb: 5.8,
+      mc: 0,
+      rt: 44,
+    },
+    trailer: "kRpkRkO9KUI",
+  },
+  {
+    name: "Parasite",
+    cover:
+      "https://www.themoviedb.org/t/p/original/xnPYcbmTN3l58uU93wcboBXD0oW.jpg",
+    description: `All unemployed, Ki-taek's family takes peculiar interest in the wealthy and glamorous Parks for their livelihood until they get entangled in an unexpected incident.`,
+    genres: ["Comedy", "Thriller", "Drama"],
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg",
+    rating: "R",
+    released: "11/01/2019 (US)",
+    runtime: "2h 13m",
+    score: {
+      imdb: 8.6,
+      mc: 0,
+      rt: 98,
+    },
+    trailer: "isOGD_7hNIY",
+  },
+  {
+    name: "Army of the Dead",
+    released: "05/14/2021 (US)",
+    genres: ["Action", "Horror", "Thriller"],
+    rating: "R",
+    runtime: "2h 28m",
+    description: `Following a zombie outbreak in Las Vegas, a group of mercenaries take the ultimate gamble: venturing into the quarantine zone to pull off the greatest heist ever attempted.`,
+    cover:
+      "https://www.themoviedb.org/t/p/original/nEPp3vCTKDtU3SZgbqhd8iVn7jZ.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/z8CExJekGrEThbpMXAmCFvvgoJR.jpg",
+    score: {
+      imdb: 5.9,
+      rt: 69,
+      mc: 0,
+    },
+    trailer: "tI1JGPhYBS8",
+  },
+  {
+    name: "The Mitchells vs. The Machines",
+    released: "04/30/2021 (CN)",
+    genres: ["Animation", "Adventure", "Comedy", "Family"],
+    rating: "PG",
+    runtime: "1h 54m",
+    description: `A quirky, dysfunctional family's road trip is upended when they find themselves in the middle of the robot apocalypse and suddenly become humanity's unlikeliest last hope.`,
+    cover:
+      "https://www.themoviedb.org/t/p/original/6ydGnzbo8s4yRNqQWPAvVSiNU8N.jpg",
+    poster:
+      "https://www.themoviedb.org/t/p/w440_and_h660_face/5TzmW7kmOhcTUe0Jecf9T1snIIT.jpg",
+    score: {
+      imdb: 7.9,
+      rt: 98,
+      mc: 0,
+    },
+    trailer: "_ak5dFt8Ar0",
+  },
+];
